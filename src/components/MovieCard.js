aeff0c61c63564e65631db091f2a02d1b92e983d
jeyakarthick44 <jeyakarthick408@gmail.com>
2022-03-08 14:25:48 +0530

    Completed , next Slider

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -6,7 +6,7 @@ const MovieCard = (props) => {
   const { data } = props;
   return (
     <div className="card-item">
-      <Link to={`/movie/${data.imdbID}`}>
+      <Link to={`/movie/${data.imdbID}`} style={{ textDecoration: "none" }}>
         <div className="card-inner">
           <div className="card-top">
             <img src={data.Poster} alt={data.Title} />
